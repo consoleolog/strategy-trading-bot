cffc43f5dc84ec5c59e8cdde54a2e02424d9eab8
consoleolog <leopportunity5@gmail.com>
2026-03-06 15:03:36 +0900

    feat: TradeCandidate 데이터 클래스 추가
    - 여러 시그널에서 발생된 거래 후보 데이터를 담을 TradeCandidate dataclass 생성
    - 가격에 관한 데이터는 Decimal 로 변환
    
    close #6

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -2,5 +2,6 @@ from .candle import Candle
 from .order import Order
 from .signal import Signal
 from .ticker import Ticker
+from .trade_candidate import TradeCandidate
 
-__all__ = ["Candle", "Order", "Signal", "Ticker"]
+__all__ = ["Candle", "Order", "Signal", "Ticker", "TradeCandidate"]
